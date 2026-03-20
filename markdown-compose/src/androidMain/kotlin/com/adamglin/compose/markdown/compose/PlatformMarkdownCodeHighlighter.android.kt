2836a6e81339633fde7d93673b6588d686c37091
adamglin <dev@adamglin.com>
2026-03-20 10:26:03 +0800

    Remove `PlatformMarkdownCodeHighlighter.android` as unused

--- markdown-compose/src/androidMain/kotlin/com/adamglin/compose/markdown/compose/PlatformMarkdownCodeHighlighter.android.kt
+++
@@ -1,8 +0,0 @@
-package com.adamglin.compose.markdown.compose
-
-import androidx.compose.runtime.Composable
-
-@Composable
-internal actual fun rememberPlatformMarkdownCodeHighlighter(
-    palette: MarkdownCodeHighlightPalette,
-): CodeHighlighter = PlainCodeHighlighter
